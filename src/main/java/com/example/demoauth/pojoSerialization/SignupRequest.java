966e12fab4bf388dd7a0d139fca6de7db976f7b7
minsort <minsort@yandex.ru>
2022-05-14 20:19:48 +0300

    first commit

---
+++ src/main/java/com/example/demoauth/pojoSerialization/SignupRequest.java
@@ -0,0 +1,4 @@
+package com.example.demoauth.pojoSerialization;
+
+public class SignupRequest {
+}
---
+++ src/main/java/com/example/demoauth/service/UserDetailsServiceImpl.java
@@ -0,0 +1,22 @@
+package com.example.demoauth.service;
+
+import com.example.demoauth.model.User;
+import com.example.demoauth.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository
+                .findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Пользователь с именем: " + username +" не найден"));
+        return UserDetailsImpl.build(user);
+    }
+}
