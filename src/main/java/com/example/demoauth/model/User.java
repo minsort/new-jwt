2abe9e39ba286090c31e665adfcac863f9acb51e
minsort <minsort@yandex.ru>
2022-05-16 15:49:28 +0300

    final backend commit

--- src/main/java/com/example/demoauth/model/User.java
+++ src/main/java/com/example/demoauth/model/User.java
@@ -26,7 +26,7 @@ public class User {
     private String email;
     private String password;
 
-    @ManyToMany(fetch = FetchType.LAZY)
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name="user_roles",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
