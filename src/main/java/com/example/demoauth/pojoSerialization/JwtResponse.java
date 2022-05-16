467e7ebedcd3b57abb2562b22d07d93b041ade04
minsort <minsort@yandex.ru>
2022-05-16 12:28:10 +0300

    final commit2

--- src/main/java/com/example/demoauth/pojoSerialization/JwtResponse.java
+++ src/main/java/com/example/demoauth/pojoSerialization/JwtResponse.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 
 import java.util.List;
 
-@AllArgsConstructor
+
 @NoArgsConstructor
 @Data
 public class JwtResponse {
@@ -16,5 +16,14 @@ public class JwtResponse {
     private Long id;
     private String username;
     private String email;
-    private List<Role> roles;
+    private List<String> roles;
+
+    public JwtResponse(String token, Long id, String username, String email, List<String> roles) {
+        this.token = token;
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.roles = roles;
+    }
+
 }
